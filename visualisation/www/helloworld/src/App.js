ccf984739c350139c3cbf5fc9c36d4abfad98072
Katie elizabeth French <845855192@bun2-206-16.local>
2018-08-30 10:39:42 +1200

    react js create element function

--- visualisation/www/helloworld/src/App.js
+++ visualisation/www/helloworld/src/App.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from '/Person/Person'
 
 const message = "yoza"
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {message}
-        </p>
-        <Welcome></Welcome>
-      </div>
-    );
+    return React.createElement("div", null, React.createElement("h1", null, "This is a react app"), React.createElement("p", null, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
   }
-}
-
-class Welcome extends App{
-    render(){
-        return(
-            <div>
-                <p>Learning ReactJS</p>
-            </div>
-        )
-    }
 }
 export default App;
