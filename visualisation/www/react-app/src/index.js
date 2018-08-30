e2655e31bdc0edf6075122bfdfa2af7a1536ffa4
Katie elizabeth French <845855192@bun2-206-16.local>
2018-08-31 08:59:54 +1200

    shopping list react app - dynamic list item name

--- visualisation/www/react-app/src/index.js
+++ visualisation/www/react-app/src/index.js
@@ -6,6 +6,27 @@ import Form from './form.js';
 
 //component - things that are changing
 class App extends Component {
+
+    constructor () {
+        super();
+        this.state = {
+            list: [
+                {
+                    id: 1,
+                    item: "Apples"
+                },
+                {
+                    id: 2,
+                    item: "Pears"
+                },
+                {
+                    id: 3,
+                    item: "Bananas"
+                }
+            ]
+        }
+    }
+
     render () {
         //inside the return is where you write JSX
         //the component needs a parent div every time
@@ -13,7 +34,7 @@ class App extends Component {
             <div>
                 <div className="jumbotron text-center">
                     <h1 className="display-4">Shopping List</h1>
-                    <ShoppingList/>
+                    <ShoppingList list = {this.state.list}/>
                     <hr/>
                     <Form/>
                 </div>
@@ -27,9 +48,12 @@ class ShoppingList extends Component{
         return(
             <div>
                 <ul className="list-group">
-                    <li className="list-group-item">First item</li>
-                    <li className="list-group-item">Second item</li>
-                    <li className="list-group-item">Third item</li>
+                    {
+                        this.props.list.map(product => {
+                            return <li className="list-group-item">{product.item}</li>
+                        })
+                    }
+
                 </ul>
             </div>
         )
