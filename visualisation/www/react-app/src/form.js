f77a792b357e580e63d1eac07001acfb2e2ddfc7
Katie elizabeth French <845855192@bun2-206-16.local>
2018-09-03 10:02:30 +1200

    react shopping app - edit and updating list item working

--- visualisation/www/react-app/src/form.js
+++ visualisation/www/react-app/src/form.js
@@ -5,6 +5,7 @@ class Form extends Component {
     constructor(props){
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     render(){
@@ -12,10 +13,10 @@ class Form extends Component {
             <div>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
-                        <input type="text" ref="newItem" placeholder="Add something to your list.." className="form-control"/>
+                        <input type="text" ref="newItem" placeholder="Add something to your list.." value={this.props.editingValue} onChange={this.onChange} className="form-control"/>
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary my-1 btn-lg btn-block">Add Item</button>
+                        <button type="submit" className="btn btn-primary my-1 btn-lg btn-block">{this.props.buttonText}</button>
                     </div>
                 </form>
             </div>
@@ -28,9 +29,25 @@ class Form extends Component {
             alert("Please enter a new item");
             return;
         }
-        this.props.addNew(newItem);
+        //if edit id is 0 - means that we are adding a new item
+        //if edit id is anything else - means that we are editing and existing item
+        if(this.props.editID === 0){
+            this.props.addNew(newItem);
+        } else{
+            var updatingItem = {
+                id: this.props.editID,
+                item: newItem
+            }
+            this.props.updateItem(updatingItem);
+        }
+
         this.refs.newItem.value = "";
     }
+
+    onChange(e){
+        this.props.changeText(e.target.value);
+    }
+
 }
 //only have to write this line if we are exporting a component from a different page
 export default Form;
--- visualisation/www/react-app/src/index.js
+++ visualisation/www/react-app/src/index.js
@@ -1,3 +1,4 @@
+
 //telling it that we want to import react and component packages
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
@@ -26,10 +27,18 @@ class App extends Component {
             ],
             text: "Hello World",
             jumboClass: "jumbotron text-center",
-            darkTheme: true
+            darkTheme: true,
+            editID: 0,
+            buttonText: "Add new item",
+            editingValue:""
         }
+
+        //you must bind everything that changes on the page
         this.changeText = this.changeText.bind(this);
         this.addNewItemToList = this.addNewItemToList.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleChangeText = this.handleChangeText.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
 
     render () {
@@ -40,9 +49,17 @@ class App extends Component {
                 <div className={this.state.jumboClass}>
                     <h1 className="display-4">Shopping List</h1>
                     <h3>{this.state.text}</h3>
-                    <ShoppingList list = {this.state.list}/>
+                    <ShoppingList
+                        list = {this.state.list}
+                        editItem={this.handleEdit}
+                    />
                     <hr/>
-                    <Form addNew= {this.addNewItemToList}/>
+                    <Form
+                        addNew = {this.addNewItemToList}
+                        {...this.state}
+                        updateItem = {this.handleUpdate}
+                        changeText = {this.handleChangeText}
+                    />
                     <button onClick={this.changeText} >Change state of h3</button>
                 </div>
             </div>
@@ -75,6 +92,40 @@ class App extends Component {
             list: this.state.list.concat(newItem)
         })
     }
+
+    handleEdit(itemToEdit){
+        this.setState({
+            editID: itemToEdit.id,
+            buttonText: "Editing item",
+            editingValue: itemToEdit.item
+        })
+    }
+
+    handleDelete(){
+
+    }
+
+    handleUpdate(updatedItem){
+        var allItems = this.state.list;
+        for (var i = 0; i < allItems.length; i++) {
+            if (allItems[i].id === updatedItem.id) {
+                allItems[i].item = updatedItem.item;
+                break;
+            }
+        }
+        this.setState({
+            list: allItems,
+            editID: 0,
+            buttonText: "Add new item",
+            editingValue: ""
+        })
+    }
+
+    handleChangeText(inputValue){
+        this.setState({
+            editingValue: inputValue
+        })
+    }
 }
 
 class ShoppingList extends Component{
@@ -84,13 +135,24 @@ class ShoppingList extends Component{
                 <ul className="list-group">
                     {
                         this.props.list.map(product => {
-                            return <li key={product.id} product={product} className="list-group-item">{product.item}</li>
+                            return <li key={product.id} product={product} className="list-group-item item">{product.item}<span><span className="edit" onClick={this.edit.bind(this, product)}>Edit</span><span className="delete" onClick={this.delete.bind(this, product)}>Delete</span></span>
+
+                            </li>
                         })
                     }
 
                 </ul>
             </div>
         )
+    }
+
+    edit(product){
+        console.log("edit");
+        this.props.editItem(product);
+    }
+    delete(product){
+        console.log("delete");
+        console.log(product);
     }
 }
 
